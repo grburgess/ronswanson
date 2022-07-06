43444cf33e7485f4852f5e11ede1b026d650b0a4
grburgess <jmichaelburgess@gmail.com>
2022-07-06 09:22:45 +0200

    typo

--- ronswanson/test/conftest.py
+++ ronswanson/test/conftest.py
@@ -5,7 +5,7 @@ from glob import glob
 from pathlib import Path
 
 import pytest
-from ronswanson.utils.package_utils import get_path_of_data_file
+from ronswanson.utils.package_data import get_path_of_data_file
 
 
 @pytest.fixture(scope="session")
