52b8b51972320ac45a467312925fe90e51c4ed0c
grburgess <jmichaelburgess@gmail.com>
2022-07-08 13:08:24 +0200

    typo

--- ronswanson/test/test_sim_builder.py
+++ ronswanson/test/test_sim_builder.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-from ronswanson import database
+# from ronswanson import database
 from ronswanson.simulation_builder import (
     Parameter,
     ParameterGrid,
