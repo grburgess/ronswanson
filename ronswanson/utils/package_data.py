78e3c478bd6bdf78086ed4f029df86a9cf309d50
grburgess <jmichaelburgess@gmail.com>
2022-07-10 16:18:32 +0200

    cleanup unneeded things

--- ronswanson/utils/package_data.py
+++ ronswanson/utils/package_data.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from shutil import copyfile
 
 import pkg_resources
 
@@ -30,62 +29,20 @@ def get_path_of_data_file(data_file: str) -> Path:
     return file_path
 
 
-def copy_package_data(data_file: str) -> None:
-    """
-    copy file from the package data directory
-    to the current directory
+# def get_path_of_user_config() -> Path:
+#     """
+#     get the path to the user configuration
 
-    :param data_file: the name of the file
-    :type data_file: str
-    :returns:
+#     :returns:
 
-    """
-    data_file_path: Path = get_path_of_data_file(data_file)
-    copyfile(data_file_path, f"./{data_file}")
+#     """
+#     config_path: Path = Path().home() / ".config" / "ronswanson"
 
+#     if not config_path.exists():
 
-def get_path_of_log_dir() -> Path:
-    """
-    return the path of the logging directory
+#         config_path.mkdir(parents=True)
 
-    :returns:
-
-    """
-    p: Path = Path("~/.log/ronswanson").expanduser()
-
-    if not p.exists():
-
-        p.mkdir(parents=True)
-
-    return p
-
-
-def get_path_of_log_file(log_file: str) -> Path:
-    """
-    returns the path of a log file
-
-    :param log_file: the name of the log file
-    :type log_file: str
-    :returns:
-
-    """
-    return get_path_of_log_dir() / log_file
-
-
-def get_path_of_user_config() -> Path:
-    """
-    get the path to the user configuration
-
-    :returns:
-
-    """
-    config_path: Path = Path().home() / ".config" / "ronswanson"
-
-    if not config_path.exists():
-
-        config_path.mkdir(parents=True)
-
-    return config_path
+#     return config_path
 
 
 __all__ = [
