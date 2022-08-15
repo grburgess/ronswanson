4530ca6fdd97cc5e143e1ad39ce34a05345a7d32
grburgess <jmichaelburgess@gmail.com>
2022-08-15 14:55:00 +0200

    change wait times

--- ronswanson/simulation.py
+++ ronswanson/simulation.py
@@ -86,7 +86,7 @@ class Simulation(metaclass=ABCMeta):
                     f"simulation {self._simulation_id} is waiting on file to be closed"
                 )
 
-                time.sleep(np.random.randint(1, 10))
+                time.sleep(np.random.uniform(3., 10))
 
             else:
 
@@ -135,7 +135,7 @@ class Simulation(metaclass=ABCMeta):
                     f"simulation {self._simulation_id} is waiting on file to be closed"
                 )
 
-                time.sleep(np.random.randint(1, 5))
+                time.sleep(np.random.uniform(3., 5.))
 
             else:
 
