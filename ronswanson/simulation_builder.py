09f3794e193887e20b018c4c2b60340439f48f03
Michael Burgess <jburgess@stargazer.local>
2022-09-16 08:08:41 +0200

    add debug info

--- ronswanson/simulation_builder.py
+++ ronswanson/simulation_builder.py
@@ -110,14 +110,59 @@ class SimulationBuilder:
 
     @classmethod
     def from_yaml(cls, file_name: str) -> "SimulationBuilder":
+        """
+        Create a simulation setup from a yaml file. The file
+        structure should follow:
+
+        ----
+
+        import_line: "from my_pkg import MySimulation as Simulation"
+        parameter_grid: my_grid.yml
+        out_file: my_database.h5
+
+        simulation:
+          n_mp_jobs: 9
+          n_cores_per_node: 72
+          use_nodes: yes
+
+          time:
+            hrs: 1
+            min: 15
+            sec: 0
+
+        # if running on a cluster
+
+        gather:
+          n_gather_per_core: 2
+          n_cores_per_node: 50
+
+          time:
+            hrs: 0
+            min: 10
+            sec: 0
+
+        ----
+
+
+        """
+
 
         with Path(file_name).open("r") as f:
 
             inputs = yaml.load(stream=f, Loader=yaml.SafeLoader)
 
+        log.debug("reading setup inputs:")
+
+        for k,v in inputs.items():
+
+            log.debug(f"{k}: {v}")
+
+
         parameter_grid = ParameterGrid.from_yaml(inputs.pop("parameter_grid"))
 
         simulation_input = inputs.pop("simulation")
+
+
 
         if "time" in simulation_input:
 
